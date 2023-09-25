b8f855b2ef6bdb2e2e58d558c2066d0e1c2faeb1
Nanoka <stornblood6969@gmail.com>
2023-09-24 21:28:14 -0500

    Breakpoint desde 740px

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -1,3 +1,3 @@
 <footer class="flex flex-wrap items-center justify-center py-4 space-x-4 dark:bg-gray-900 text-md dark:text-gray-100 home-footer">
-Diseñado por GRUPO 5 - 2023
+Diseñado por Carlos Escate - 2023
 </footer>
