02c9fa3c49207ffbc34704a72b10f95a31d0f4b3
Nanoka <stornblood6969@gmail.com>
2023-10-15 23:07:18 -0500

    Modified footer

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -1,3 +1,3 @@
 <footer class="flex flex-wrap items-center justify-center py-4 space-x-4 border-t border-gray-100 dark:border-gray-900 dark:bg-gray-900 text-md dark:text-gray-100 home-footer">
-    © 2023 | Carlos Escate
+    © 2023 | Grupo 5 TPW
 </footer>
